e623e60690c68d07a908f80948edba666f6da151
Duk3-C <orlandojmarotogarcia@proton.me>
2026-07-18 15:25:23 -0400

    Refactorized the approach to this problem

--- prog.c
+++ prog.c
@@ -1,35 +1,46 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
-#include <stdbool.h>
+#include <string.h>
 
-srand(time(NULL));
+#define TRUE 1
+#define FALSE 0
 
 int main(int argc, char *argv[])
 {
+  int loop = TRUE;
+  srand(time(NULL));
 
-  bool loop = true;
+  char students[] = {"Juan Marcello", "Hector Jimenez", "Arthur Stain", "Paulina Rodriguez", "Kamala Harryson", "Joshua Gutierrez"};
 
-  while( loop )
+  while (loop) 
   {
+    printf("---------------------------\n"); 
+    printf("-----Student Selection-----\n"); 
+    printf("---------------------------\n\n"); 
+    printf("1) Initialize Selector\n");
+    printf("2) View students list\n");
+    printf("3) View available roles\n");
+    printf("q/Q) Quit program\n\n");
+
     char in_menu;
-    scanf("%s", &in_menu);
+    scanf("%c", &in_menu);
 
-    if(in_menu == "1")
+    switch (in_menu) 
     {
+      case 1:
+      case 2:
+      case 3:
+      case 'q':
+        loop = FALSE;
+      case 'Q':
+        loop = FALSE;
+      default:
+        printf("Invalid Answer, try again");
+        continue;
+    }
+  }
 
-    } 
-    else if(in_menu == "q" || in_menu == "Q")
-    {
-      printf("Exiting the program...\n");
-      loop = false;
-    }
-    else {
-      printf("Invalid Answer\n");
-      continue;
-    }
-    
-  };
   return 0;
 
 }
