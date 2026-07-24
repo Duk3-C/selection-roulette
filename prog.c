c833b08e8d9a712b0f0bf754c4fe49b0a107e05a
Duk3-C <orlandojmarotogarcia@proton.me>
2026-07-23 23:05:10 -0400

    pluh

--- prog.c
+++ prog.c
@@ -6,12 +6,26 @@
 #define TRUE 1
 #define FALSE 0
 
+void shuffle(char **arr, int n)
+{
+  for (int i = n - 1; i > 0; --i) 
+  {
+    int j = rand() % (i + 1);
+    char *t = arr[i]; arr[i] = arr[j]; arr[j] = t; 
+  }
+}
+
 int main(int argc, char *argv[])
 {
   int loop = TRUE;
   srand(time(NULL));
 
-  char students[] = {"Juan Marcello", "Hector Jimenez", "Arthur Stain", "Paulina Rodriguez", "Kamala Harryson", "Joshua Gutierrez"};
+  char *students[] = {"Juan Marcello", "Hector Jimenez", "Arthur Stain", "Paulina Rodriguez", "Kamala Harryson", "Joshua Gutierrez"};
+  char *roles[] = {"Live Developer", "Exercise Facilitator"};
+  const char* selector_buffer[4];
+
+  int num_students = sizeof(students) / sizeof(students[0]);
+  int num_roles = sizeof(roles) / sizeof(roles[0]);  
 
   while (loop) 
   {
@@ -29,6 +43,11 @@ int main(int argc, char *argv[])
     switch (in_menu) 
     {
       case 1:
+        for (int i=0; i>num_students; ++i) 
+        {  
+          shuffle(students, num_students);
+          printf("%s\n", students[i]);
+        }
       case 2:
       case 3:
       case 'q':
